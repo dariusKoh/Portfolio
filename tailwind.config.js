0e7fa602aa316339d39c8f020f674070571d8554
dariusKoh <55723854+dariusKoh@users.noreply.github.com>
2022-07-11 00:33:25 +0800

    setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/index.html"],
+  content: ['./index.html'],
+  
   theme: {
-    //Setup of the Fonts
-    fontFamily: {
-      Lato: ["Lato", "sans-serif"],
-      Poppins: ["Poppins", "sans-serif"],
-      Montserrat: ["Montserrat", "sans-serif"],
+    extend: { //adding my own themes
+      //Setup of the Fonts
+      fontFamily: {
+        'Lato': ['Lato', 'sans-serif'],
+        'Poppins': ['Poppins', 'sans-serif'],
+        'Montserrat': ['Montserrat', 'sans-serif'],
+        },
+  
+      //Setup of the Colours
+      colors: {
+        body: '#17171F',
+        selectedtext: '#A3A3FF',
+        sitetheme: '#3F3FFF',
+        nav: '#404053',
+        secondary: '#9191A4',
+        badge: '#3F3F51',
+        inputborder: '#565666',
+        input: '#2A2A35',
+      },
     },
-    //Setup of the Colours
-    colors: {
-      body: "#17171F",
-      "selected-text": "#A3A3FF",
-      theme: "#3F3FFF",
-      nav: "#404053",
-      secondary: "#9191A4",
-      badge: "#3F3F51",
-      "input-border": "#565666",
-      input: "#2A2A35",
-    },
-    extend: {},
   },
-  plugins: [],
-};
+
+}
