d639599247c03a25642962af51886adf31d320a7
dariusKoh <55723854+dariusKoh@users.noreply.github.com>
2022-07-12 02:29:06 +0800

    Added modals for Competitions, Enrichment and Projects

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.{html,js}'],
+  content: [
+    "./*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js"],
   theme: {
-     extend: { //adding my own themes
+    extend: {
+      //adding my own themes
       //Setup of the Fonts
       fontFamily: {
-        'Lato': ['Lato', 'sans-serif'],
-        'Poppins': ['Poppins', 'sans-serif'],
-        },
-  
+        Lato: ["Lato", "sans-serif"],
+        Poppins: ["Poppins", "sans-serif"],
+      },
+
       //Setup of the Colours
       colors: {
-        headc: '#101010',
-        bodyc: '#141416',
-        selectedtext: '#A3A3FF',
-        sitetheme: '#3F3FFF',
-        nav: '#404053',
-        badsecondary: '#9191A4',
-        badbadge: '#3F3F51',
-        badinputborder: '#565666',
-        input: '#2A2A35',
+        headc: "#101010",
+        bodyc: "#141416",
+        selectedtext: "#A3A3FF",
+        sitetheme: "#3F3FFF",
+        nav: "#404053",
+        badsecondary: "#9191A4",
+        badbadge: "#3F3F51",
+        badinputborder: "#565666",
+        input: "#2A2A35",
       },
     },
   },
-  plugins: [require('@tailwindcss/line-clamp')],
-}
+  plugins: [
+    require("@tailwindcss/line-clamp"),
+    require("tw-elements/dist/plugin"),
+  ],
+};
